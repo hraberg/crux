75a7502980943f91a67938dfce939809955d1d35
Håkan Råberg <hakan.raberg@gmail.com>
2020-11-16 21:49:21 +0100

    Tweaking to use Crux codec.

--- crux-core/src/crux/range.clj
+++ crux-core/src/crux/range.clj
@@ -5,6 +5,7 @@
            java.nio.charset.StandardCharsets
            [java.util NavigableSet TreeSet])
   (:require [clojure.string :as str]
+            [crux.codec :as c]
             [crux.memory :as mem]))
 
 ;; "Adaptive Range Filters for Cold Data: Avoiding Trips to Siberia"
@@ -28,12 +29,6 @@
 (defn long->buffer ^org.agrona.DirectBuffer [^long x]
   (doto ^MutableDirectBuffer (mem/allocate-buffer Long/BYTES)
     (.putLong 0 x ByteOrder/BIG_ENDIAN)))
-
-(defn long->prefix ^String [^long x]
-  (str/replace (String. (mem/->on-heap (long->buffer x)) StandardCharsets/UTF_8) #"\u0000*$" ""))
-
-(defn str->long ^long [x]
-  (buffer->long (mem/as-buffer (.getBytes (str x) StandardCharsets/UTF_8))))
 
 (defn seek-bitmap [^Roaring64Bitmap bm ^long k]
   (let [rank (.rankLong bm k)]
@@ -65,70 +60,83 @@
 
 (deftype FilteredSet [^NavigableSet s ^Roaring64Bitmap bm])
 
-(defn ->fs
-  (^crux.range.FilteredSet [] (->fs (TreeSet.)))
-  (^crux.range.FilteredSet [^NavigableSet s]
-   (FilteredSet. s (Roaring64Bitmap.))))
+(defn ->fs ^crux.range.FilteredSet []
+  (FilteredSet. (TreeSet. mem/buffer-comparator) (Roaring64Bitmap.)))
 
-(defn fs-add ^crux.range.FilteredSet [^FilteredSet fs k]
+(defn fs-add ^crux.range.FilteredSet [^FilteredSet fs ^DirectBuffer k]
   (.add ^NavigableSet (.s fs) k)
-  (insert-key (.bm fs) (str->long k))
+  (insert-key (.bm fs) (buffer->long k))
   fs)
 
-(defn fs-seek-potential-k [^FilteredSet fs k]
-  (let [k-long (str->long k)]
+(defn fs-seek-potential-k ^org.agrona.DirectBuffer [^FilteredSet fs ^DirectBuffer k]
+  (let [k-long (buffer->long k)]
     (if (range-may-contain? (.bm fs) k-long)
       k
       (when-let [next-k (seek-bitmap (.bm fs) k-long)]
-        (long->prefix next-k)))))
+        (let [next-k-buffer ^DirectBuffer (long->buffer next-k)]
+          (if (= @#'c/string-value-type-id (.getByte next-k-buffer 0))
+            (loop [n 0]
+              (cond
+                (= n Long/BYTES)
+                next-k-buffer
+                (= @#'c/string-terminate-mark (.getByte next-k-buffer n))
+                (mem/limit-buffer next-k-buffer (inc n))
+                :else
+                (recur (inc n))))
+            next-k-buffer))))))
 
-(defn fs-seek [^FilteredSet fs k]
+(defn fs-seek ^org.agrona.DirectBuffer [^FilteredSet fs ^DirectBuffer k]
   (when-let [k-probe (fs-seek-potential-k fs k)]
     (when-let [found (first (.tailSet ^NavigableSet (.s fs) k-probe))]
       (when-not (= k found)
         (insert-empty-range (.bm fs)
-                            (str->long k)
-                            (str->long found)))
+                            (buffer->long k)
+                            (buffer->long found)))
       found)))
 
-(defn fs-contains? [^FilteredSet fs k]
-  (let [k-long (str->long k)]
+(defn fs-contains? [^FilteredSet fs ^DirectBuffer k]
+  (let [k-long (buffer->long k)]
     (and (range-may-contain? (.bm fs) k-long)
          (= k (fs-seek fs k)))))
 
 (comment
-  (let [fs (->fs (TreeSet. ["f",
-                            "far",
-                            "fast",
-                            "s",
-                            "top",
-                            "toy",
-                            "trie"]))]
-    (assert (= "f" (fs-seek fs "a")))
-    (assert (= ["a" "f"] (map long->prefix (iterator-seq (.iterator ^Roaring64Bitmap (.bm fs))))))
-    (assert (= "f" (fs-seek fs "b")))
-    (assert (= ["a" "f"] (map long->prefix (iterator-seq (.iterator ^Roaring64Bitmap (.bm fs))))))
-    (fs-add fs "c")
-    (fs-add fs "g")
-    (assert (true? (fs-contains? fs "c")))
-    (assert (= ["a" "c"
-                (long->prefix (unchecked-inc (str->long "c")))  "f"]
-               (map long->prefix (iterator-seq (.iterator ^Roaring64Bitmap (.bm fs))))))
-    (assert (= "top" (fs-seek fs "t")))
-    (assert (= "top" (fs-seek fs "to")))
-    (assert (false? (fs-contains? fs "too")))
-    (assert (true? (fs-contains? fs "toy")))
-    (assert (= ["a" "c"
-                (long->prefix (unchecked-inc (str->long "c")))  "f"
-                "t" "top"]
-               (map long->prefix (iterator-seq (.iterator ^Roaring64Bitmap (.bm fs))))))
+  (let [fs ^FilteredSet (reduce fs-add
+                                (->fs)
+                                (->>  ["f"
+                                       "far"
+                                       "fast"
+                                       "s"
+                                       "top"
+                                       "toy"
+                                       "trie"]
+                                      (mapv c/->value-buffer)))]
+    (assert (= "f" (c/decode-value-buffer (fs-seek fs (c/->value-buffer "a")))))
+    (assert (= [604327874509406208 605735249392959488]
+               (iterator-seq (.iterator ^Roaring64Bitmap (.bm fs)))))
+    (assert (= "f" (c/decode-value-buffer (fs-seek fs (c/->value-buffer "b")))))
+    (assert (= [604327874509406208 605735249392959488]
+               (iterator-seq (.iterator ^Roaring64Bitmap (.bm fs)))))
+    (fs-add fs (c/->value-buffer "c"))
+    (fs-add fs (c/->value-buffer "g"))
+    (assert (true? (fs-contains? fs (c/->value-buffer "c"))))
+    (assert (= [604327874509406208 604890824462827520
+                604890824462827521 605735249392959488]
+               (iterator-seq (.iterator ^Roaring64Bitmap (.bm fs)))))
+    (assert (= "top" (c/decode-value-buffer (fs-seek fs (c/->value-buffer "t")))))
+    (assert (= "top" (c/decode-value-buffer (fs-seek fs (c/->value-buffer "to")))))
+    (assert (false? (fs-contains? fs (c/->value-buffer "too"))))
+    (assert (true? (fs-contains? fs (c/->value-buffer "toy"))))
+    (= [604327874509406208 604890824462827520
+        604890824462827521 605735249392959488
+        609675899066908672 609799534012268544]
+       (iterator-seq (.iterator ^Roaring64Bitmap (.bm fs))))
 
     (assert (= ["c",
-                "f",
-                "far",
-                "fast",
-                "g",
-                "s",
-                "top",
-                "toy",
-                "trie"] (vec (.s fs))))))
+                "f"
+                "far"
+                "fast"
+                "g"
+                "s"
+                "top"
+                "toy"
+                "trie"] (mapv c/decode-value-buffer (.s fs))))))
